3516e382c25c703ba4a46a65d9d5765e5ed96188
Sokolov Nikita <nia40m@yandex.ru>
2017-02-26 19:22:33 +0300

    Lab2: Add verification of user with UID
    Signed-off-by: Sokolov Nikita <nia40m@yandex.ru>

--- lab2/main.c
+++ lab2/main.c
@@ -3,23 +3,37 @@
 #include <linux/types.h>
 #include <linux/kdev_t.h>
 #include <linux/fs.h>
+#include <linux/cred.h>
 
-#define MAI_DEVICE_NUMBERS 1
+#define MAI_SUPERUSER_UID 0
+#define MAI_BUFF_SIZE_DEFAULT 128
+#define MAI_MAJOR_NUMBER 666
+#define MAI_DEVICE_NAME "mai_pipe"
 
-MODULE_LICENSE("GPL");
-MODULE_DESCRIPTION("nia40m's implementation of MAI_PIPE");
-MODULE_AUTHOR("nia40m");
-
-static int buff_size = 128;
+static int buff_size = MAI_BUFF_SIZE_DEFAULT;
 module_param(buff_size, int, 0000);
 
-dev_t m_nums = MKDEV(666, 666);
+/* defenition of all file_operation functions */
+static int mai_pipe_open(struct inode *inode, struct file *file);
+/* end of defenition*/
+
+/* fops for all users */
+static struct file_operations mai_pipe_fops = {
+    .owner = THIS_MODULE,
+    .open  = mai_pipe_open,
+};
+/* fops for superuser
+static struct file_operations mai_pipe_fops_superuser = {
+
+};
+*/
+
 
 static int __init mai_pipe_init(void)
 {
     int status;
 
-    status = register_chrdev_region(m_nums, MAI_DEVICE_NUMBERS, "mai_pipe");
+    status = register_chrdev(MAI_MAJOR_NUMBER, MAI_DEVICE_NAME, &mai_pipe_fops);
     if (status < 0)
         return -1;
 
@@ -28,8 +42,27 @@ static int __init mai_pipe_init(void)
 
 static void __exit mai_pipe_exit(void)
 {
-    unregister_chrdev_region(m_nums, MAI_DEVICE_NUMBERS);
+    unregister_chrdev(MAI_MAJOR_NUMBER, MAI_DEVICE_NAME);
 }
+
+static int mai_pipe_open(struct inode *inode, struct file *file)
+{
+    const struct cred *cred;
+    int uid;
+
+    /* getting credential to obtain process real UID */
+    cred = get_cred(file->f_cred);
+    uid  = cred->uid.val;
+    put_cred(cred);
+
+    return -1;
+}
+
+
 
 module_init(mai_pipe_init);
 module_exit(mai_pipe_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("nia40m's implementation of MAI_PIPE");
+MODULE_AUTHOR("nia40m");
