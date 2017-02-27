6f8879d1ff7ea7c8355e0890c2139175117a40d8
Sokolov Nikita <nia40m@yandex.ru>
2017-02-27 20:26:08 +0300

    Lab2: Fix some style warnings
    Signed-off-by: Sokolov Nikita <nia40m@yandex.ru>

--- lab2/main.c
+++ lab2/main.c
@@ -37,20 +37,20 @@ static ssize_t mai_pipe_write_superuser
 /* end of defenition */
 
 /* fops for all users */
-static struct file_operations mai_pipe_fops = {
-    .owner   = THIS_MODULE,
-    .open    = mai_pipe_open,
-    .release = mai_pipe_close,
-    .read    = mai_pipe_read,
-    .write   = mai_pipe_write,
+static const struct file_operations mai_pipe_fops = {
+	.owner   = THIS_MODULE,
+	.open    = mai_pipe_open,
+	.release = mai_pipe_close,
+	.read    = mai_pipe_read,
+	.write   = mai_pipe_write,
 };
 
 /* fops for superuser */
-static struct file_operations mai_pipe_fops_superuser = {
-    .owner = THIS_MODULE,
-    .open  = mai_pipe_open,
-    .read  = mai_pipe_read_superuser,
-    .write = mai_pipe_write_superuser,
+static const struct file_operations mai_pipe_fops_superuser = {
+	.owner = THIS_MODULE,
+	.open  = mai_pipe_open,
+	.read  = mai_pipe_read_superuser,
+	.write = mai_pipe_write_superuser,
 };
 
 
@@ -59,252 +59,255 @@ static wait_queue_head_t wait_queue;
 static struct list_head list_head;
 
 struct buffer {
-    struct list_head list;
-    int   uid;
-    int   opened;
-    char *buff;
-    int   num;
-    int   off_start;
-    int   off_end;
+	struct list_head list;
+	int   uid;
+	int   opened;
+	char *buff;
+	int   num;
+	int   off_start;
+	int   off_end;
 };
 
 static int __init mai_pipe_init(void)
 {
-    int status;
+	int status;
 
-    status = register_chrdev(MAI_MAJOR_NUMBER, MAI_DEVICE_NAME, &mai_pipe_fops);
-    if (status < 0)
-        return -1;
+	status = register_chrdev(MAI_MAJOR_NUMBER,
+		MAI_DEVICE_NAME, &mai_pipe_fops);
+	if (status < 0)
+		return -1;
 
-    INIT_LIST_HEAD(&list_head);
-    init_waitqueue_head(&wait_queue);
+	INIT_LIST_HEAD(&list_head);
+	init_waitqueue_head(&wait_queue);
 
-    return 0;
+	return 0;
 }
 
 static void __exit mai_pipe_exit(void)
 {
-    struct list_head *ptr1, *ptr2;
-    struct buffer *temp;
+	struct list_head *ptr1, *ptr2;
 
-    unregister_chrdev(MAI_MAJOR_NUMBER, MAI_DEVICE_NAME);
+	unregister_chrdev(MAI_MAJOR_NUMBER, MAI_DEVICE_NAME);
 
-    list_for_each_safe(ptr1, ptr2, &list_head) {
-        temp = list_entry(ptr1, struct buffer, list);
-        list_del(ptr1);
-        kfree(temp->buff);
-        kfree(temp);
-    }
+	list_for_each_safe(ptr1, ptr2, &list_head) {
+		struct buffer *temp;
+
+		temp = list_entry(ptr1, struct buffer, list);
+		list_del(ptr1);
+		kfree(temp->buff);
+		kfree(temp);
+	}
 }
 
 static int mai_pipe_open(struct inode *inode, struct file *file)
 {
-    const struct cred *cred;
-    int uid;
-    struct list_head *ptr;
-    struct buffer *temp, *curr = NULL;
+	const struct cred *cred;
+	int uid;
+	struct list_head *ptr;
+	struct buffer *temp, *curr = NULL;
 
-    /* getting credential to obtain process real UID */
-    cred = get_cred(file->f_cred);
-    uid  = cred->uid.val;
-    put_cred(cred);
+	/* getting credential to obtain process real UID */
+	cred = get_cred(file->f_cred);
+	uid  = cred->uid.val;
+	put_cred(cred);
 
-    /* replacement of fops for superuser */
-    if (uid == MAI_SUPERUSER_UID) {
-        file->f_op = &mai_pipe_fops_superuser;
-        return 0;
-    }
+	/* replacement of fops for superuser */
+	if (uid == MAI_SUPERUSER_UID) {
+		file->f_op = &mai_pipe_fops_superuser;
+		return 0;
+	}
 
-    /* searching for buffer based on uid */
-    list_for_each(ptr, &list_head) {
-        temp = list_entry(ptr, struct buffer, list);
+	/* searching for buffer based on uid */
+	list_for_each(ptr, &list_head) {
+		temp = list_entry(ptr, struct buffer, list);
 
-        if (uid == temp->uid) {
-            curr = temp;
-            break;
-        }
-    }
+		if (uid == temp->uid) {
+			curr = temp;
+			break;
+		}
+	}
 
-    /* create buffer if doesn't exist */
-    if (curr == NULL) {
-        curr = (struct buffer *) kmalloc(sizeof(struct buffer), GFP_KERNEL);
-        if (curr == NULL)
-            return -1;
+	/* create buffer if doesn't exist */
+	if (curr == NULL) {
+		curr = kmalloc(sizeof(struct buffer), GFP_KERNEL);
+		if (curr == NULL)
+			return -1;
 
-        curr->buff = (char *) kmalloc(buff_size, GFP_KERNEL);
-        if (curr->buff == NULL) {
-            kfree(curr);
-            return -1;
-        }
+		curr->buff = kmalloc(buff_size, GFP_KERNEL);
+		if (curr->buff == NULL) {
+			kfree(curr);
+			return -1;
+		}
 
-        curr->uid = uid;
-        curr->num = curr->off_start = curr->off_end = curr->opened = 0;
+		curr->uid = uid;
+		curr->num = curr->off_start = curr->off_end = curr->opened = 0;
 
-        list_add(&(curr->list), &list_head);
-    }
+		list_add(&(curr->list), &list_head);
+	}
 
-    curr->opened++;
-    file->private_data = curr;
+	curr->opened++;
+	file->private_data = curr;
 
-    return 0;
+	return 0;
 }
 
 static int mai_pipe_close(struct inode *inode, struct file *file)
 {
-    struct buffer *curr;
+	struct buffer *curr;
 
-    curr = (struct buffer *) file->private_data;
-    curr->opened--;
+	curr = (struct buffer *) file->private_data;
+	curr->opened--;
 
-    if (curr->num || curr->opened) {
-        wake_up_interruptible(&wait_queue);
-        return 0;
-    }
+	if (curr->num || curr->opened) {
+		wake_up_interruptible(&wait_queue);
+		return 0;
+	}
 
-    list_del(&(curr->list));
-    kfree(curr->buff);
-    kfree(curr);
+	list_del(&(curr->list));
+	kfree(curr->buff);
+	kfree(curr);
 
-    return 0;
+	return 0;
 }
 
 static void read_from_buffer(struct buffer *from, char *to, size_t size)
 {
-    int i, j = 0;
+	int i, j = 0;
 
-    for (i = from->off_start; j < size; i = (i + 1) % buff_size)
-        to[j++] = from->buff[i];
+	for (i = from->off_start; j < size; i = (i + 1) % buff_size)
+		to[j++] = from->buff[i];
 
-    from->off_start = (from->off_start + size) % buff_size;
-    from->num -= size;
+	from->off_start = (from->off_start + size) % buff_size;
+	from->num -= size;
 }
 
 static ssize_t mai_pipe_read
 (struct file *file, char *buff, size_t size, loff_t *off)
 {
-    int status;
-    size_t actual_size;
-    char *temp_buff;
-    struct buffer *curr;
+	int status;
+	size_t actual_size;
+	char *temp_buff;
+	struct buffer *curr;
 
-    curr = (struct buffer *) file->private_data;
+	curr = (struct buffer *) file->private_data;
 
-    temp_buff = (char *) kmalloc(size, GFP_KERNEL);
-    if (temp_buff == NULL)
-        return -1;
+	temp_buff = kmalloc(size, GFP_KERNEL);
+	if (temp_buff == NULL)
+		return -1;
 
-    actual_size = (curr->num >= size) ? size : curr->num;
+	actual_size = (curr->num >= size) ? size : curr->num;
 
-    read_from_buffer(curr, temp_buff, actual_size);
+	read_from_buffer(curr, temp_buff, actual_size);
 
-    wake_up_interruptible(&wait_queue);
+	wake_up_interruptible(&wait_queue);
 
-    while (actual_size < size && curr->opened > 1) {
-        int temp_size;
+	while (actual_size < size && curr->opened > 1) {
+		int temp_size;
 
-        wait_event_interruptible(wait_queue,
-            curr->num > 0 || curr->opened == 1);
+		wait_event_interruptible(wait_queue,
+			curr->num > 0 || curr->opened == 1);
 
-        temp_size = size - actual_size;
+		temp_size = size - actual_size;
 
-        if (curr->num > 0)
-            temp_size = (temp_size > curr->num) ? curr->num : temp_size;
-        else
-            break;
+		if (curr->num > 0)
+			temp_size =
+				(temp_size > curr->num) ? curr->num : temp_size;
+		else
+			break;
 
-        read_from_buffer(curr, temp_buff + actual_size, temp_size);
-        actual_size += temp_size;
+		read_from_buffer(curr, temp_buff + actual_size, temp_size);
+		actual_size += temp_size;
 
-        wake_up_interruptible(&wait_queue);
-    }
+		wake_up_interruptible(&wait_queue);
+	}
 
-    status = copy_to_user(buff, temp_buff, actual_size);
+	status = copy_to_user(buff, temp_buff, actual_size);
 
-    kfree(temp_buff);
+	kfree(temp_buff);
 
-    if (status)
-        return -1;
+	if (status)
+		return -1;
 
-    *off += actual_size;
-    return actual_size;
+	*off += actual_size;
+	return actual_size;
 }
 
 static void write_to_buffer(struct buffer *to, char *from, size_t size)
 {
-    int i, j = 0;
+	int i, j = 0;
 
-    for (i = to->off_end; j < size; i = (i + 1) % buff_size)
-        to->buff[i] = from[j++];
+	for (i = to->off_end; j < size; i = (i + 1) % buff_size)
+		to->buff[i] = from[j++];
 
-    to->off_end = (to->off_end + size) % buff_size;
-    to->num += size;
+	to->off_end = (to->off_end + size) % buff_size;
+	to->num += size;
 }
 
 static ssize_t mai_pipe_write
 (struct file *file, const char *buff, size_t size, loff_t *off)
 {
-    int status;
-    int free;
-    size_t actual_size;
-    char *temp_buff;
-    struct buffer *curr;
+	int status;
+	int free;
+	size_t actual_size;
+	char *temp_buff;
+	struct buffer *curr;
 
-    curr = (struct buffer *) file->private_data;
+	curr = (struct buffer *) file->private_data;
 
-    temp_buff = (char *) kmalloc(size, GFP_KERNEL);
-    if (temp_buff == NULL)
-        return -1;
+	temp_buff = kmalloc(size, GFP_KERNEL);
+	if (temp_buff == NULL)
+		return -1;
 
-    status = copy_from_user(temp_buff, buff, size);
-    if (status) {
-        kfree(temp_buff);
-        return -1;
-    }
+	status = copy_from_user(temp_buff, buff, size);
+	if (status) {
+		kfree(temp_buff);
+		return -1;
+	}
 
-    free = buff_size - curr->num;
-    actual_size = (free >= size) ? size : free;
+	free = buff_size - curr->num;
+	actual_size = (free >= size) ? size : free;
 
-    write_to_buffer(curr, temp_buff, actual_size);
+	write_to_buffer(curr, temp_buff, actual_size);
 
-    wake_up_interruptible(&wait_queue);
+	wake_up_interruptible(&wait_queue);
 
-    while (actual_size < size) {
-        int temp_size;
+	while (actual_size < size) {
+		int temp_size;
 
-        wait_event_interruptible(wait_queue, curr->num < buff_size);
+		wait_event_interruptible(wait_queue, curr->num < buff_size);
 
-        temp_size = size - actual_size;
-        free = buff_size - curr->num;
+		temp_size = size - actual_size;
+		free = buff_size - curr->num;
 
-        temp_size = (temp_size > free) ? free : temp_size;
+		temp_size = (temp_size > free) ? free : temp_size;
 
-        write_to_buffer(curr, temp_buff + actual_size, temp_size);
-        actual_size += temp_size;
+		write_to_buffer(curr, temp_buff + actual_size, temp_size);
+		actual_size += temp_size;
 
-        wake_up_interruptible(&wait_queue);
-    }
+		wake_up_interruptible(&wait_queue);
+	}
 
-    kfree(temp_buff);
-    *off += actual_size;
-    return actual_size;
+	kfree(temp_buff);
+	*off += actual_size;
+	return actual_size;
 }
 
 static ssize_t mai_pipe_read_superuser
 (struct file *file, char *buff, size_t size, loff_t *off)
 {
-    pr_info("Superuser is trying to read info! "
-        "Ha-ha, RETARD! YOU CAN'T DO SUCH THING!\n");
+	pr_info("Superuser is trying to read info!");
+	pr_info("Ha-ha, RETARD! YOU CAN'T DO SUCH THING!\n");
 
-    return 0;
+	return 0;
 }
 
 static ssize_t mai_pipe_write_superuser
 (struct file *file, const char *buff, size_t size, loff_t *off)
 {
-    pr_info("Superuser is trying to write info! "
-        "USE LINUX KERNEL PIPE, SCALLYWAG!\n");
+	pr_info("Superuser is trying to write info!");
+	pr_info("USE LINUX KERNEL PIPE, SCALLYWAG!\n");
 
-    return (ssize_t) size;
+	return (ssize_t) size;
 }
 
 
