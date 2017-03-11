4924a1d34091f04013a18ae6f63274fef34579ca
Sokolov Nikita <nia40m@yandex.ru>
2017-03-11 19:01:04 +0300

    Lab3-part2: Updated test.c file
    Signed-off-by: Sokolov Nikita <nia40m@yandex.ru>

--- lab3/p2/test.c
+++ lab3/p2/test.c
@@ -8,24 +8,30 @@ void print(void *smt)
 
 void main()
 {
-    n4_queue myq;
+    n4_queue myq, queue2;
 
     n4_init(&myq);
 
-    n4_add(&myq, print, "Function 1");
-    n4_add(&myq, print, "Function 2");
+    n4_add(&myq, print, "Thread 1: Function 1");
+    n4_add(&myq, print, "Thread 1: Function 2");
 
     printf("Main thread: msg 1\n");
 
     n4_work(&myq);
 
-    n4_add(&myq, print, "Function 3");
+    n4_add(&myq, print, "Thread 1: Function 3");
+
+    n4_init(&queue2);
+    n4_add(&queue2, print, "Thread 2: Function 1");
+    n4_add(&queue2, print, "Thread 2: Function 2");
 
     printf("Main thread: msg 2\n");
 
     n4_work(&myq);
+    n4_work(&queue2);
 
     n4_exit(&myq);
+    n4_exit(&queue2);
 
     printf("Main thread: msg 3\n");
 }
